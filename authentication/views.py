588b9bf27f94cf61851820ab2e17a8499d21a092
Harsh K. Nagarkar <hknagark@go.olemiss.edu>
2019-03-04 16:09:32 -0600

    Dashboard

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import User,UserProfile
 
+from django.http import HttpResponsePermanentRedirect
 # Create your views here.
 
-def home(request):
-		return render(request, 'home.html')
-		#return render(request, 'loginPageIndex.html')
-		
-#def log(request):
-#		return render(request, 'loginPageIndex.html')
-#def password_reset(request):
-#		return render(request, 'home.html')
+# def home(request):
+# 		return render(request, 'home.html')
+
+def userView(request):
+	# if request.user.is_authenticated():
+	return render(request, 'dashboard/userdashboard.html')
+	# else:
+		# return HttpResponsePermanentRedirect("/login")
