d065797e47a451b9680a08994c1e3167f4113f58
Harsh K. Nagarkar <hknagark@go.olemiss.edu>
2019-04-02 00:01:48 -0500

    css fixed

--- foodmachine/views.py
+++ foodmachine/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 import SENDGRIDkey
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 from django.views import generic
 # Create your views here.
 
