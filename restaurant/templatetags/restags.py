6e07933e828c540d0db48d8a608ebae98792ee85
Harsh K. Nagarkar <hknagark@go.olemiss.edu>
2019-04-14 16:22:25 -0500

    background image fix

--- restaurant/templatetags/restags.py
+++ restaurant/templatetags/restags.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from itertools import chain
 # Create your views here.
 from restaurant.models import Restaurant, Menu, Cuisine, Review
+from authentication.models import UserProfile,User
 
 register = template.Library()
 
@@ -12,3 +13,10 @@ def getMenu(resID):
     print(resID)
     context = Menu.objects.filter(Menu_Res_Id=resID)
     return context
+
+
+@register.filter(name='getProfile')
+def getProfile(usr):
+    print(usr)
+    context = User.objects.get(pk = (User.objects.get(username=usr).id))
+    return context.userprofile.__dict__
